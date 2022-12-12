ef31c75c7bf689dd00537800b4d0836e1085d099
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2022-12-12 18:56:40 +0100

    login
    testat lite olika för login, fungerar dock inte

--- PHP/login.php
+++ PHP/login.php
@@ -2,32 +2,32 @@
 
 require_once "functions.php";
 
-$filename = "PHP/user.json";
-$requestMethod = $_SERVER["REQUEST_METHOD"];
+$loginUser = [];
 
-if ($requestMethod != "GET") {
-    $error = ["error" => "Invalid HTTP method! (Only GET is allowed)"];
-    sendJSON($error, 405);
-}
-
-$users = [];
-
-if (file_exists($filename)) {
-    $json = file_get_contents($filename);
+if (isset($_GET["username"]) && isset($_GET["password"])) {
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    $json = file_get_contents("PHP/user.json");
     $users = json_decode($json, true);
+
+    foreach ($users as $user) {
+        $loginUser = $user[$username] && $user[$password];
+    }
 }
 
-$requestJSON = file_get_contents("php://input");
-$requestData = json_decode($requestJSON, true);
+// $filename = "PHP/user.json";
+// $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-$username = $requestData["username"];
-$password = $requestData["password"];
+// if ($requestMethod != "GET") {
+//     $error = ["error" => "Invalid HTTP method! (Only GET is allowed)"];
+//     sendJSON($error, 405);
+// }
 
-// fungerar inte, men ska gå igenom alla users för att se om användarnamnet stämmer 
-// for($users as $user) {
-//     if ($username == $valueUsername && $password == $valuePassword) {
-    
-//     }
+// $users = [];
+
+// if (file_exists($filename)) {
+//     $json = file_get_contents($filename);
+//     $users = json_decode($json, true);
 // }
 
 ?>
@@ -46,9 +46,9 @@ $password = $requestData["password"];
     <form id="loginForm" >
         <h3>Logga in</h3>
         <label>Användarnamn</label>
-        <input type="text" name="username" id="username" placeholder="Användarnamn" value="<?php $valueUsername ?>">
+        <input type="text" name="username" id="username" placeholder="Användarnamn">
         <label>Lösenord</label>
-        <input type="password" name="password" id="password" placeholder="Lösenord" value="<?php $valuePassword ?>">
+        <input type="password" name="password" id="password" placeholder="Lösenord">
         <input type="submit" value="Send" id="submitUser">
         <input type="submit" value="Skapa konto" id="registerNewUser">
     </form>
