ab865bd7eaa68019d5b7d8ad6cf5947619668c26
Joakim Fröjvall Bejram <joakimfrojram@gmail.com>
2022-12-20 11:39:14 +0100

    add_castle

--- index.php
+++ index.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="CSS/home.css">
-    <link rel="stylesheet" href="CSS/register.css">
-    <link rel="stylesheet" href="CSS/profile.css">
-    <link rel="stylesheet" href="CSS/movies.css">
-    <link rel="stylesheet" href="CSS/login.css">
-    <link rel="stylesheet" href="CSS/welcome.css">
-    <link rel="stylesheet" href="CSS/index.css">
-    <link rel="stylesheet" href="CSS/popUp.css">
+    <link rel="stylesheet" type="text/css" href="CSS/home.css">
+    <link rel="stylesheet" type="text/css" href="CSS/register.css">
+    <link rel="stylesheet" type="text/css" href="CSS/profile.css">
+    <link rel="stylesheet" type="text/css" href="CSS/movies.css">
+    <link rel="stylesheet" type="text/css" href="CSS/login.css">
+    <link rel="stylesheet" type="text/css" href="CSS/welcome.css">
+    <link rel="stylesheet" type="text/css" href="CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="CSS/popUp.css">
     <title>Disney Mystery Club</title>
   </head>
   <body>
+    <div class="paris"> <img src="Bilder\paris.png" alt=""> </div>
+    <wrapper 
+    id="loginPage"  class="hideloginPage">
 
-    <wrapper id="loginPage"  class="hideloginPage">
       <form action="PHP/login.php" id="loginForm" method="GET">
           <h3>Logga in</h3>
           <label>Användarnamn</label>
