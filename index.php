4edb203f6648bed48ab713ce448b52cf8c9af2e5
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2022-12-22 09:37:21 +0100

    hjärta på like-knappen

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" type="text/css" href="CSS\welcome.css">
     <link rel="stylesheet" type="text/css" href="CSS\index.css">
     <link rel="stylesheet" type="text/css" href="CSS\popUp.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Disney Mystery Club</title>
   </head>
   <body>
